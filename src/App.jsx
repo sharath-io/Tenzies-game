51e67b30043e1ed852a15d2497a375298df22c58
Sharath <sharath.esr@gmail.com>
2025-02-04 18:17:55 +0530

    feat: build dice hold & roll feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,50 @@ export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return new Array(10)
-       .fill(0)
-       .map(() => ({
-          value:Math.floor(Math.random() * 6) + 1,
-          isHeld:false,
-          id:nanoid()
+    return new Array(10).fill(0).map(() => ({
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid(),
     }));
   }
 
-  const diceElements = dice.map((diebObj) => 
-  (   <Die key={diebObj.id} value={diebObj.value} isHeld={diebObj.isHeld} id={diebObj.id} />
+  const hold = (id) => {
+    setDice((oldDice) =>
+      oldDice.map((dice) =>
+        dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice
+      )
+    );
+  };
 
+  const diceElements = dice.map((diebObj) => (
+    <Die
+      key={diebObj.id}
+      value={diebObj.value}
+      isHeld={diebObj.isHeld}
+      id={diebObj.id}
+      hold={() => hold(diebObj.id)}
+    />
   ));
 
-  const handleRoll =() => setDice(generateAllNewDice())
+  const rollDice = () =>
+    setDice((oldDice) =>
+      oldDice.map((dice) =>
+        dice.isHeld ? dice : { ...dice, value: Math.floor(Math.random() * 6)+1 }
+      )
+    );
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
 
-      <button className="roll-dice" onClick={handleRoll}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,13 @@
-
 export default function Die(props) {
 
   return (
     <main>
-      <button style={{backgroundColor:props.isHeld? "#59E391" : ""}}>{props.value}</button>
+      <button
+        style={{ backgroundColor: props.isHeld ? "#59E391" : "" }}
+        onClick={props.hold}
+      >
+        {props.value}
+      </button>
     </main>
-  )
+  );
 }
