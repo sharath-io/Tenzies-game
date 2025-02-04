c5636ea104cbadda91c678ee93623e025962dabd
Sharath <sharath.esr@gmail.com>
2025-02-04 16:31:43 +0530

    feat: generated random numbers on the dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-import './App.css'
-import Die from './components/Die'
+import { useState } from "react";
+import "./App.css";
+import Die from "./components/Die";
 
 export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.floor(Math.random() * 6) + 1);
+  }
+
+  const diceElements = dice.map((num) => <Die value={num} />);
 
   return (
     <main>
-
-    <div className="dice-container">
-
-    
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    <Die value={1}/>
-    </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
-  )
+  );
 }
